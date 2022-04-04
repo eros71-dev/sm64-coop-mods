3df25840871b936ab56cf7dc33da30fdb3d73f90
eros71-dev <16540103+eros71-dev@users.noreply.github.com>
2022-04-04 04:47:32 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -4,22 +4,26 @@
 --- @param m MarioState
 function mario_on_set_action(m)
     if (m.action == ACT_JUMP) then -- if mario jumps
-        set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND)
+        set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND) -- replace animation, not working
+        set_anim_to_frame(m, 1)
         print("jumped")
     end
 
     if (m.action == ACT_DOUBLE_JUMP) then -- if mario double jumps
-        set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND)
+        set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND) -- replace animation, not working
+        set_anim_to_frame(m, 1)
         print("double jumped")
     end
 
     if (m.action == ACT_TRIPLE_JUMP) then -- if mario triple jumps
-        set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND)
+        set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND) -- replace animation, not working
+        set_anim_to_frame(m, 1)
         print("triple jumped")
     end
 
-    if (m.action == ACT_LONG_JUMP) then -- if mario long jumps
-        set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND)
+    if (m.action == ACT_LONG_JUMP) then -- if mario long jumps, not working
+        set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND) -- replace animation, not working
+        set_anim_to_frame(m, 1)
         print("long jumped")
     end
 end
