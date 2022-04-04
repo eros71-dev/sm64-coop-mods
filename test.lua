c3a7ced5669b630381dffb45a503d22fcfa974f4
eros71-dev <16540103+eros71-dev@users.noreply.github.com>
2022-04-04 04:37:56 +0200

    Add unfinished test mod

---
+++ test.lua
@@ -0,0 +1,20 @@
+-- name: test
+-- description: test
+--- @param m MarioState
+function mario_on_set_action(m)
+    if (m.action == ACT_JUMP) then -- if mario jumps
+        set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND)
+        print("jumped")
+    end
+    if (m.action == ACT_DOUBLE_JUMP) then -- if mario double jumps
+        set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND)
+        print("double jumped")
+    end
+    if (m.action == ACT_TRIPLE_JUMP) then -- if mario triple jumps
+        set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND)
+        print("triple jumped")
+    end
+end
+
+-- hooks --
+hook_event(HOOK_ON_SET_MARIO_ACTION, mario_on_set_action)
