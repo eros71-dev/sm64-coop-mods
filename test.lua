c6a28dc06870b88d535252f6dae94822dbc640fd
eros71-dev <16540103+eros71-dev@users.noreply.github.com>
2022-04-04 05:06:36 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,16 +1,16 @@
--- name: test
--- description: test
+-- name: Mario cap test
+-- description: Makes you "lose your cap" (not working yet) and lets you put it back by crouching (that does actually work) 
 -- unfinished, and i'm tired as I wrote this so expect dumb code mistakes and grammar mistakes
 --- @param m MarioState
 function mario_on_set_action(m)
 
     if (m.action == ACT_TRIPLE_JUMP) then -- if mario triple jumps
-        mario_blow_off_cap(m, 1) --maker mario lose his cap, no idea on how to actually make the cap fly off his head
+        mario_blow_off_cap(m, 4) --supposedly make mario lose his cap, no idea on how to actually make the cap fly off his head, it just does nothing
         print("triple jumped")
     end
 
-    if (m.action == ACT_START_CROUCHING) then -- if mario starts crouching
-        mario_retrieve_cap(m) --mario gets his cap back, he doesnt put it on though, i need to fix that
+    if (m.action == ACT_START_CROUCHING or m.action == ACT_CROUCH_SLIDE) then -- if mario starts crouching or starts sliding as he crouches
+        mario_retrieve_cap(m) --mario gets his cap back, holy shit it works now
         print("crouched")
     end
 
