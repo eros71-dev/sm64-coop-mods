2e3b52df4cb15ab6b943216b38a80453b891175c
eros71-dev <16540103+eros71-dev@users.noreply.github.com>
2022-04-04 04:40:20 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -7,13 +7,20 @@ function mario_on_set_action(m)
         set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND)
         print("jumped")
     end
+
     if (m.action == ACT_DOUBLE_JUMP) then -- if mario double jumps
         set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND)
         print("double jumped")
     end
+
     if (m.action == ACT_TRIPLE_JUMP) then -- if mario triple jumps
         set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND)
         print("triple jumped")
+    end
+
+    if (m.action == ACT_LONG_JUMP) then -- if mario long jumps
+        set_mario_animation(m, MARIO_ANIM_TRIPLE_JUMP_LAND)
+        print("long jumped")
     end
 end
 
