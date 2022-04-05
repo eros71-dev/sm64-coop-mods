38891062005a66f56c906f8c935e407eb09d9c2c
eros71-dev <16540103+eros71-dev@users.noreply.github.com>
2022-04-05 03:00:02 +0200

    Update the mod again

--- test.lua
+++ test.lua
@@ -4,15 +4,17 @@
 --- @param m MarioState
 function mario_on_set_action(m)
 
-    if (m.action == ACT_TRIPLE_JUMP) then -- if mario triple jumps
-        mario_blow_off_cap(m, 4) --supposedly make mario lose his cap, no idea on how to actually make the cap fly off his head, it just does nothing
+    if (m.action == ACT_TRIPLE_JUMP or m.action == ACT_RELEASING_BOWSER or m.action == ACT_SOFT_BONK) then -- if mario triple jumps
+        mario_blow_off_cap(m, 4) --supposedly make the player lose their cap
+        -- no idea on how to actually make the cap fly off his head, it just does nothing
+        play_character_sound(m, CHAR_SOUND_WHOA)
         print("triple jumped")
     end
 
-    if (m.action == ACT_START_CROUCHING or m.action == ACT_CROUCH_SLIDE) then -- if mario starts crouching or starts sliding as he crouches
-        mario_retrieve_cap(m) --mario gets his cap back, holy shit it works now
-        print("crouched")
-    end
+    --if (m.action == ACT_START_CROUCHING or m.action == ACT_CROUCH_SLIDE) then -- if mario starts crouching or starts sliding as he crouches
+    --    mario_retrieve_cap(m)
+    --    print("crouched")
+    --end
 
     if (m.action == ACT_STOP_CROUCHING) then -- if mario finishes crouching
         cutscene_put_cap_on(m) -- make mario put his cap on
