006f55b8550e8b83c126519d482a4e74da18ba82
eros71-dev <16540103+eros71-dev@users.noreply.github.com>
2022-04-07 02:31:12 +0200

    Prepare networking and clean-up.
    Prepare for networking, remove unnecessarry boolean, and add unfinished cap removal function.

--- loose-cap.lua
+++ loose-cap.lua
@@ -1,40 +1,35 @@
 -- name: Loose cap
 -- description: Your cap is not sautered onto your skull correctly anymore.\n\nSpecial thanks to Agent X for the name, mod by eros71.
 
-local capDropped = false;
 local localMario = gMarioStates[0];
 
-function mario_on_set_action(m)
+function mario_on_set_action(localMario)
 
-    if (m.action == ACT_START_CROUCHING and m.flags & ~MARIO_CAP_IN_HAND) then
+    if (localMario.action == ACT_START_CROUCHING and localMario.flags & MARIO_CAP_IN_HAND) then --aways exectutes when the player crouches,
+        -- no idea why
         cutscene_put_cap_on(localMario)
     end
 
     if (
-        m.action == ACT_RELEASING_BOWSER or 
-        m.action == ACT_SOFT_BONK or 
-        m.action == ACT_DEATH_ON_BACK or
-        m.action == ACT_THROWN_BACKWARD or
-        m.action == ACT_FALL_AFTER_STAR_GRAB or
-        m.action == ACT_DISAPPEARED or
-        m.action == ACT_FREEFALL
+        localMario.action == ACT_RELEASING_BOWSER or 
+        localMario.action == ACT_SOFT_BONK or 
+        localMario.action == ACT_DEATH_ON_BACK or
+        localMario.action == ACT_THROWN_BACKWARD or
+        localMario.action == ACT_FALL_AFTER_STAR_GRAB or
+        localMario.action == ACT_DISAPPEARED or
+        localMario.action == ACT_FREEFALL
+        and localMario.flags & ~MARIO_CAP_IN_HAND
     ) then
-        mario_blow_off_cap(localMario, 5)
-        capDropped = true;
-        --TODO: Figure out how to make the player not take the cap back right away as soon as it gets off their head.
-        play_character_sound(localMario, CHAR_SOUND_WHOA)
+        mario_blow_off_cap(localMario, 5);
+        play_character_sound(localMario, CHAR_SOUND_WHOA);
     end
 
 end
 
 -- Retrieve cap command, non-functional right now.
-function on_retrieve_command()
-    --TODO: Check if cap was dropped already from the player's head, and if so, make this remove *this player's* cap from the floor.
-    -- Not that it matters too much right now since probably nothing is networked yet.
-    if capDropped then
-        
-    end
+function on_retrieve_command(localMario)
     if (localMario.flags & ~MARIO_CAP_ON_HEAD) then --if the retrieve command is executed and mario is not wearing a cap
+        obj_mark_for_deletion(cap) --TODO: Delete the cap from the level once it's given back to the player. How do I actually mark the cap?
         mario_retrieve_cap(localMario)
         cutscene_put_cap_on(localMario)
         return true
