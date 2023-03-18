087998c296a3e2146247b8950941ccdcc9503f17
eros71-dev <16540103+eros71-dev@users.noreply.github.com>
2023-03-18 21:14:28 +0100

    Update main.lua

--- odyssey-moon-jingle/main.lua
+++ odyssey-moon-jingle/main.lua
@@ -1,4 +1,4 @@
 -- name: Odyssey Star Fanfare Mod
--- description: yes, by eros71 or something idk
+-- description: Replaces the SM64 Star Get Fanfare with the Super Mario Odyssey one, but in the SM64 style.\nRecommended to be used along with Isaac's OMM mod!
 
 smlua_audio_utils_replace_sequence(SEQ_EVENT_CUTSCENE_COLLECT_STAR, 34, 100, "star")
